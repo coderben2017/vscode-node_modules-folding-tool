9b16941f8825168e2ec0b4d926e69b22d95ec6cf
jinben <jinben@sugon.com>
2023-03-08 17:07:46 +0800

    ✨ feat: 增加折叠dist、es目录功能

--- src/extension.ts
+++ src/extension.ts
@@ -5,20 +5,24 @@ const excluded = config.get('files.exclude', {} as Record<string, boolean>);
 
 const exclude = (): Thenable<any> => {
 	excluded['**/node_modules'] = true;
+	excluded['**/es'] = true;
+	excluded['**/dist'] = true;
 	return config.update('files.exclude', excluded);
-}
+};
 
 const include = (): Thenable<any> => {
 	excluded['**/node_modules'] = false;
+	excluded['**/es'] = false;
+	excluded['**/dist'] = false;
 	return config.update('files.exclude', excluded);
-}
+};
 
 export function activate(context: vscode.ExtensionContext) {
 	const command: string = 'node-modules-folder.fold';
 	const handler = async (...args: any[]): Promise<any> => {
 		await exclude();
 		await include();
-	}
+	};
 	context.subscriptions.push(vscode.commands.registerCommand(command, handler));
 }
 
